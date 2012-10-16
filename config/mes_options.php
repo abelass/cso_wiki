89b2a590d2286efe1698a9dd80b628bbbf91c95b
abelass <websolutions@mychacra.net>
2012-10-16 13:56:17 +0200

    import

---
+++ config/mes_options.php
@@ -0,0 +1,4 @@
+<?php
+$GLOBALS['forcer_lang'] = true;
+?>
+
---
+++ squelettes/lang/local_en.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add_content'=>'Add a new Resource:',
+	'add_ressource'=>'Add a Resource',
+	'advanced_search'=>'Advanced Search',
+	
+	// B
+	'best_practice'=>'Best Practice',
+	
+	// C
+	'case_study'=>'Case Study',
+	'changer_langue_contribution'=>'Change the language of this contribution',
+	'contributions'=>'Resources',
+	'contribution_usefull'=>'Rating',
+	
+	//D
+	'docs_rel'=>'Related Info',
+	
+	
+	//K
+	'lang'=>'Language',
+	
+	//M
+	'modify_contribution'=>'Edit this contribution',
+	'modify_contribution_close'=>'Stop editing',
+	'moyenne'=>'Rating',
+	
+	//N
+	'new' => 'New',
+	'new_contribution'=>'New contribution',
+	'nothing'=>'Nothing',
+		
+	//O
+	'orderby'=>'Order by:',
+	
+		
+	//P
+	'pays'=>'Country',
+	
+	//R
+	'recent_contributions'=>'Recent contributions',
+	'ressources'=>'Resources',
+	
+	//S
+    'sense_trie_ascendant'=>'Ascendant',
+    'sense_trie_descendant'=>'Descendant',    	
+	'since'=>'Since',
+	'shortcuts'=>'Shortcuts',
+	
+	//T
+	'tool'=>'Tool',
+	'type_liaison'=>'Type'
+);
+
+?>
