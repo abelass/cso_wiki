4e7defadd3ca118c2631f5492fd9bec0987dfa48
abelass <websolutions@mychacra.net>
2012-10-23 09:14:23 +0200

    traductions en fr et es

---
+++ squelettes/lang/local_es.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add_content'=>'Agregue un Recurso Nuevo:',
+	'add_ressource'=>'Agregue un Recurso',
+	'advanced_search'=>'Búsqueda avanzada',
+	
+	// B
+	'best_practice'=>'Buena práctica',
+	
+	// C
+	'case_study'=>'Estudio de caso',
+	'changer_langue_contribution'=>'Cambie el idioma de esta contribución',
+	'contributions'=>'Recursos',
+	'contribution_usefull'=>'Rating',
+	
+	//D
+	'docs_rel'=>'Más',
+	
+	
+	//K
+	'lang'=>'Idioma',
+	
+	//M
+	'modify_contribution'=>'Edite esta contribución',
+	'modify_contribution_close'=>'Terminar la edición',
+	'moyenne'=>'Rating',
+	
+	//N
+	'new' => 'Nuevo',
+	'new_contribution'=>'Nueva contribución',
+	'nothing'=>'Nada',
+		
+	//O
+	'orderby'=>'Orrdenar por :',
+	
+		
+	//P
+	'pays'=>'País',
+	
+	//R
+	'recent_contributions'=>'Contribuciones recientes',
+	'ressources'=>'Recursos',
+	
+	//S
+    'sense_trie_ascendant'=>'Ascendente',
+    'sense_trie_descendant'=>'Descendente',    	
+	'since'=>'Desde',
+	'shortcuts'=>'Atajos',
+	
+	//T
+	'tool'=>'Herramienta',
+	'type_liaison'=>'Typo'
+);
+
+?>
---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,61 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+ 
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'add_content'=>'Ajouter une nouvelle ressource:',
+	'add_ressource'=>'Ajouter une ressource',
+	'advanced_search'=>'Recherche avancée',
+	
+	// B
+	'best_practice'=>'Bonnes pratiques',
+	
+	// C
+	'case_study'=>'Cas pratique',
+	'changer_langue_contribution'=>'Changer la langue de cette contribution ',
+	'contributions'=>'Contributionbs',
+	'contribution_usefull'=>'Rating',
+	
+	//D
+	'docs_rel'=>'Info en lien',
+	
+	
+	//K
+	'lang'=>'Langues',
+	
+	//M
+	'modify_contribution'=>'Modifier cette contribution',
+	'modify_contribution_close'=>'Terminer l\'édition',
+	'moyenne'=>'Rating',
+	
+	//N
+	'new' => 'Nouveau',
+	'new_contribution'=>'Nouvelle contribution',
+	'nothing'=>'Rien',
+		
+	//O
+	'orderby'=>'Trie par :',
+	
+		
+	//P
+	'pays'=>'Pays',
+	
+	//R
+	'recent_contributions'=>'Contributions récentes',
+	'ressources'=>'Ressources',
+	
+	//S
+    'sense_trie_ascendant'=>'Ascendant',
+    'sense_trie_descendant'=>'Descendant',    	
+	'since'=>'Depuis',
+	'shortcuts'=>'Raccourcis',
+	
+	//T
+	'tool'=>'Outils',
+	'type_liaison'=>'Type'
+);
+
+?>
